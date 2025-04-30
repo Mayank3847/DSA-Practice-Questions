1eaed4459206205a61b488c830d2f1ea60c9b9ad
Mayank Shekhar <mayankshekhar0303gmail.com>
2025-04-30 14:11:13 +0530

    #

--- MaxXORWithoutTrie.java
+++ MaxXORWithoutTrie.java
@@ -26,10 +26,28 @@ public class MaxXORWithoutTrie {
 
         return maxResult;
     }
+    public static int findMaximumXORBrute(int[] nums) {
+        int maxXOR = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int xor = nums[i] ^ nums[j];
+                if (xor > maxXOR) {
+                    maxXOR = xor;
+                }
+            }
+        }
+
+        return maxXOR;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {3, 10, 5, 25, 2, 8};
         System.out.println("Maximum XOR is: " + findMaximumXOR(nums));
+        System.out.println("Maximum XOR is: " + findMaximumXORBrute(nums));
+
     }
 }
 
